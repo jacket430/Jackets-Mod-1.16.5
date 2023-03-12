834548f90d16c382b827ea0ebc8db2c4b6a8972c
jacket430 <34522242+jacket430@users.noreply.github.com>
2023-03-11 19:55:16 -0700

    Initial commit, test item only

---
+++ src/main/java/net/avemitchell/jacketsmod/item/TestItemItem.java
@@ -0,0 +1,44 @@
+
+package net.avemitchell.jacketsmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.avemitchell.jacketsmod.JacketsModModElements;
+
+@JacketsModModElements.ModElement.Tag
+public class TestItemItem extends JacketsModModElements.ModElement {
+	@ObjectHolder("jackets_mod:test_item")
+	public static final Item block = null;
+
+	public TestItemItem(JacketsModModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("test_item");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
